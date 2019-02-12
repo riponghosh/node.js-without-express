d7d55c2827731359651480f56e16585563aefa33
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-02-12 12:28:01 +0600

    server create

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var helloServer = http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World');
+})
+helloServer.listen(8001);
